ccce08a5398a7abb39484a34c89ec6a6cd3a3e9a
RushFTK <867871766@qq.com>
2018-03-21 21:00:54 +0800

    Add py3 vesion of XParser.

---
+++ crawler_scrapy/crawler_scrapy/spiders/testspider.py
@@ -0,0 +1,54 @@
+import scrapy
+
+class testspider(scrapy.Spider):
+    name = "test"
+    start_urls = ['http://tester1.409dostastudio.work/'
+                  # ]
+                  ,'http://tester2.409dostastudio.work/']
+    url_lists = []
+
+    def parse(self, response):
+        ifnonewurls = 1
+        testspider.url_lists.append(response.url)
+        #get all request page
+        urls = response.xpath('//@href').extract()
+        for path_url in urls:
+            if len(path_url.split('/'))> 1:
+                url_dirs = path_url.split('/')[1:]
+                real_url = response.url
+                for index in range(len(url_dirs)):
+                    real_url += url_dirs[index]
+                    if index != (len(url_dirs)-1):
+                        real_url += '/'
+                    if (index == (len(url_dirs)-1)) and (len(url_dirs[index].split('.'))<=1):
+                        real_url += '/'
+                self.log('Get url - %s' % real_url)
+                if not real_url in testspider.url_lists:
+                    ifnonewurls = 0
+                    yield scrapy.Request(response.urljoin(real_url))
+
+        #confirm saved page title
+        # page_name = response.url
+        # if (page_name.split('/')[-1]) is not "":
+        #     filename = 'test-%s-%s.html' % (page_name.split('/')[-3] , page_name.split('/')[-1])
+        # else:
+        #     filename = 'test-%s.html' % page_name.split('/')[-2]
+
+        #confirm saved url
+
+        if ifnonewurls:
+            with open('url_list.txt', 'w') as f:
+                for urls in testspider.url_lists:
+                    f.write(urls + '\n')
+                f.close()
+            self.log('Saved url_list')
+
+    #get context
+    def parseContent(self, response):
+        contents = response.xpath('//@input/name').extract()
+        with open('target_list.txt','w') as f:
+            for content in contents:
+                f.write(content + '\n')
+            f.close()
+        self.log('Saved target_list')
+        pass
